ee363889e1ad9da284bc072874f4245eb5ab6483
dengcheng <644403982@qq.com>
2018-12-07 17:10:11 +0800

    搭建基于 model 的卡片列表页面

--- config/config.js
+++ config/config.js
@@ -1,11 +1,38 @@
 export default {
+	singular: true,
 	plugins: [
 		['umi-plugin-react',{
-			
+			antd: true,
+			dva: true
 		}],
 	],
-	routes: [{
-		path: '/',
-		component: './HelloWorld',
-	}],
+	routes: [
+		{
+			path: '/',
+			component: '../layout',
+			routes: [
+				{
+					path: '/',
+					component: 'HelloWorld'
+				},
+				{
+					path: '/helloworld',
+        			component: 'Helloworld'
+				},
+				{
+					path: '/dashboard',
+					routes: [
+						{ path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          				{ path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          				{ path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+					]
+				},
+				{
+					path: 'puzzlecards',
+					component: './puzzlecards'
+				}
+			],
+		}
+	],
+
 }
